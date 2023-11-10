03547de34fb44e03e01358c0c308d2557cd74f8c
Fredy Sugiarto <sugiarto2fredy@gmail.com>
2023-11-11 00:30:59 +0700

    Initial Commit

---
+++ Engagement Product/Engagement_ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  Engagement_ProductApp.swift
+//  Engagement Product
+//
+//  Created by Fredy Sugiarto on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Engagement_ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
